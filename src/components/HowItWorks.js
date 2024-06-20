d7c35706df6d4358f9592e09036380c09c49d308
Rohan <rohannsahh@gmail.com>
2024-06-20 22:10:36 +0530

    20

--- src/components/HowItWorks.js
+++ src/components/HowItWorks.js
@@ -8,8 +8,8 @@ const HowItWorks = () => {
   return (
     <>
     <Header/>
-    <div className="relative bg-gray-100  flex flex-col rounded-md shadow-lg items-center">
-      <div className="relative w-full max-w-7xl h-[600px]">
+    <div className="relative  bg-gray-100 flex flex-col rounded-md shadow-lg items-center ">
+      <div className="relative hidden md:block w-full max-w-7xl h-[600px]">
       <div className="absolute inset-0">
         <img
           src={require('../assets/world.png')}
@@ -70,6 +70,61 @@ const HowItWorks = () => {
       </div>
     </div>
   
+        {/* Mobile Layout */}
+        <div className=" md:hidden flex flex-col items-center space-y-6 py-6">
+        <div className="absolute inset-0">
+        <img
+          src={require('../assets/world.png')}
+          alt="World Map"
+          className="object-cover  w-full h-full opacity-50"
+        />
+      </div>
+          <h2 className="text-2xl font-bold text-[#000080] text-center">
+            5 Steps Process to get the toughest visa the easy way
+          </h2>
+          <div className="flex flex-col items-center px-2">
+            <div className="bg-blue-700 text-white w-8 h-8 flex items-center justify-center text-xl font-bold rounded-full">1</div>
+            <div className="text-center text-base mt-2">
+              <p className="font-bold">Fill in Details:</p>
+              <p>Submit all valid information when booking your consultation.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center px-2">
+            <div className="bg-blue-700 text-white w-8 h-8 flex items-center justify-center text-xl font-bold rounded-full">2</div>
+            <div className="text-center text-base mt-2">
+              <p className="font-bold">Reviewing Your Details:</p>
+              <p>We take one business day to check and understand your application.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center px-2">
+            <div className="bg-blue-700 text-white w-8 h-8 flex items-center justify-center text-xl font-bold rounded-full">3</div>
+            <div className="text-center text-base mt-2">
+              <p className="font-bold">Personalized Consultation:</p>
+              <p>Receive tailored advice for your specific case.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center px-2">
+            <div className="bg-blue-700 text-white w-8 h-8 flex items-center justify-center text-xl font-bold rounded-full">4</div>
+            <div className="text-center text-base mt-2">
+              <p className="font-bold">Apply to the Required Visa Country:</p>
+              <p>Apply directly with the Embassy once your application is ready.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center px-2">
+            <div className="bg-blue-700 text-white w-8 h-8 flex items-center justify-center text-xl font-bold rounded-full">5</div>
+            <div className="text-center text-base mt-2">
+              <p className="font-bold">Bon Voyage:</p>
+              <p>You're all set for your journey!</p>
+            </div>
+            <div className="p-2">
+          <img src={require('../assets/airplane.png')} alt="Airplane" className="w-16 h-16    " />
+        </div>
+          </div>
+        </div>
     <Footer/>
     </>
   );
